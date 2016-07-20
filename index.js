ef780d301550af7b601cac7c935eb271a2054b58
bhupendrasingh <bhupendrasingh.2@gmail.com>
2016-07-20 19:06:02 -0400

    angular doesn't play nice with jade

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
-app.set('views', __dirname + '/tpl');
-app.set('view engine', 'pug');
-app.engine('pug', require('pug').__express);
+
+var express = require('express');
+var app = express();
+//app.set('views', __dirname + '/tpl');
+//app.set('view engine', 'pug');
+//app.engine('pug', require('pug').__express);
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req,res){
-    res.render('home');
-    //res.send("hello world");
+    res.render('index.html')
 });
+
 app.listen(8000);
 console.log("toado app server is running on port 8000");
